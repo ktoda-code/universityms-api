dad1c342b82e4f94d6f619eaae46a58039f98eec
Konstantin Todorov Andreev <ktod.andreev@gmail.com>
2023-08-04 20:22:22 +0300

    Initial commit

---
+++ src/test/java/com/ktoda/universityms/UniversityMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktoda.universityms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
